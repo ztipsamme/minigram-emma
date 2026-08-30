fce8bbf833774bc7cfb2978887d14406dce4bbdf
ztipsamme <e.spitz@hotmail.com>
2026-08-30 03:14:34 +0200

    add GetByIdAsync

--- backend/Services/ImageService.cs
+++ backend/Services/ImageService.cs
@@ -26,58 +26,30 @@ namespace backend.Services
         public async Task<List<ImageDTO>> GetAllAsync()
         {
             List<ImageDTO> res = new();
-
-            var userDelegationKey = await _serviceClient
-                .GetUserDelegationKeyAsync(
-                    DateTimeOffset.UtcNow.AddMinutes(-5),
-                    DateTimeOffset.UtcNow.AddHours(1));
+            var delegationKey = await GetUserDelegationKeyAsync();
 
             await foreach (BlobItem blobItem in _container.GetBlobsAsync(BlobTraits.Metadata))
             {
-                var metadata = blobItem.Metadata;
-
-                metadata.TryGetValue("Id", out var id);
-                metadata.TryGetValue("Caption", out var caption);
-
-
-                var tags = metadata.TryGetValue("Tags", out var t)
-                    ? t.Split(",", StringSplitOptions.RemoveEmptyEntries).ToList()
-                    : new List<string>();
-
-                var blobClient = _container.GetBlobClient(blobItem.Name);
-
-                var sasBuilder = new Azure.Storage.Sas.BlobSasBuilder
-                {
-                    BlobContainerName = _container.Name,
-                    BlobName = blobItem.Name,
-                    Resource = "b",
-                    StartsOn = DateTimeOffset.UtcNow.AddMinutes(-5),
-                    ExpiresOn = DateTimeOffset.UtcNow.AddHours(1)
-                };
-                sasBuilder.SetPermissions(Azure.Storage.Sas.BlobSasPermissions.Read);
-
-                var sasToken = sasBuilder.ToSasQueryParameters(
-                    userDelegationKey,
-                    _container.AccountName
-                ).ToString();
-
-                var sasUri = $"{blobClient.Uri}?{sasToken}";
-
-                res.Add(new ImageDTO(
-                    Id: id,
-                    Name: blobItem.Name,
-                    Caption: caption ?? string.Empty,
-                    Tags: tags,
-                    Url: sasUri.ToString()
-                ));
+                var dto = MapToImageDTO(blobItem.Name, blobItem.Metadata, delegationKey);
+                res.Add(dto);
             }
 
             return res;
         }
 
-        public async Task<Image> GetByIdAsync(string id)
+        public async Task<ImageDTO?> GetByIdAsync(string id)
         {
-            throw new NotImplementedException();
+            var delegationKey = await GetUserDelegationKeyAsync();
+
+            await foreach (BlobItem blobItem in _container.GetBlobsAsync(BlobTraits.Metadata))
+            {
+                if (blobItem.Metadata.TryGetValue("Id", out var blobId) && blobId == id)
+                {
+                    return MapToImageDTO(blobItem.Name, blobItem.Metadata, delegationKey);
+                }
+            }
+
+            return null;
         }
 
         public async Task<Image> CreateImageAsync(NewImage newImage)
@@ -93,6 +65,56 @@ namespace backend.Services
         public async Task<bool> DeleteImageByIdAsync(string id)
         {
             throw new NotImplementedException();
+        }
+
+        private async Task<UserDelegationKey> GetUserDelegationKeyAsync()
+        {
+            return await _serviceClient
+                .GetUserDelegationKeyAsync(
+                    DateTimeOffset.UtcNow.AddMinutes(-5),
+                    DateTimeOffset.UtcNow.AddHours(1));
+        }
+
+        private ImageDTO MapToImageDTO(string blobName, IDictionary<string, string> metadata, UserDelegationKey delegationKey)
+        {
+            metadata.TryGetValue("Id", out var id);
+            metadata.TryGetValue("Caption", out var caption);
+
+            var tags = metadata.TryGetValue("Tags", out var t)
+                ? t.Split(",", StringSplitOptions.RemoveEmptyEntries).ToList()
+                : new List<string>();
+
+            var sasUri = GenerateSasUrl(blobName, delegationKey);
+
+            return new ImageDTO(
+                    Id: id,
+                    Name: blobName,
+                    Caption: caption ?? string.Empty,
+                    Tags: tags,
+                    Url: sasUri.ToString()
+                );
+        }
+
+        private string GenerateSasUrl(string blobName, UserDelegationKey delegationKey)
+        {
+            var blobClient = _container.GetBlobClient(blobName);
+
+            var sasBuilder = new Azure.Storage.Sas.BlobSasBuilder
+            {
+                BlobContainerName = _container.Name,
+                BlobName = blobName,
+                Resource = "b",
+                StartsOn = DateTimeOffset.UtcNow.AddMinutes(-5),
+                ExpiresOn = DateTimeOffset.UtcNow.AddHours(1)
+            };
+            sasBuilder.SetPermissions(Azure.Storage.Sas.BlobSasPermissions.Read);
+
+            var sasToken = sasBuilder.ToSasQueryParameters(
+                delegationKey,
+                _container.AccountName
+            ).ToString();
+
+            return $"{blobClient.Uri}?{sasToken}";
         }
     }
 }
